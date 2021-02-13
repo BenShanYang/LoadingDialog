0205ef7a6de7d600611037db9c554330a65aa0fe
BenShanYang <1805197091@qq.com>
2021-02-13 13:15:18 +0800

    添加示例图片

--- app/src/main/java/com/yk/loadingdialog/MainActivity.java
+++ app/src/main/java/com/yk/loadingdialog/MainActivity.java
@@ -15,12 +15,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         LoadingDialog.Builder loadBuilder = new LoadingDialog.Builder(this)
-                .setMessage("加载中...")
-                .setCancelable(true)
-                .setMessageColor(Color.WHITE)
-                .setMessageSize(14)
-                .setBackgroundTransparent(true)
-                .setCancelOutside(true);
+                .setMessage("加载中...")//设置提示文字
+                .setCancelable(true)//按返回键取消
+                .setMessageColor(Color.WHITE)//提示文字颜色
+                .setMessageSize(14)//提示文字字号
+                .setBackgroundTransparent(true)//弹窗背景色是透明或半透明
+                .setCancelOutside(true);//点击空白区域弹消失
         LoadingDialog dialog = loadBuilder.create();
         dialog.show();
     }
